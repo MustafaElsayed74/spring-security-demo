9766d1312782506c9b8ddc503a997dada147d318
0x0desha74 <mudesha74@gmail.com>
2025-07-19 21:13:16 +0300

    JDBC users accounts - make spring security use tables in database

--- 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/config/SecurityConfig.java
+++ 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/config/SecurityConfig.java
@@ -8,40 +8,49 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
     // InMemory Users: means every time the app run, the users are created and stored in memory [RAM] not the DB [desk]
     //Spring will not user the default user/pass or the existed in the app.prop, instead spring will use this InMemory Users
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//
+//
+//        UserDetails loz = User.builder()
+//                .username("loz")
+//                .password("{noop}loz123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//
+//        UserDetails loza = User.builder()
+//                .username("loza")
+//                .password("{noop}loza123")
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//
+//        UserDetails desha = User.builder()
+//                .username("desha")
+//                .password("{noop}mudesha") //noop [Encryption Algorithm]=> No Operation {plain text}
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//
+//
+//        return new InMemoryUserDetailsManager(loz, loza, desha);
+//    }
+
+    //this will allow spring security to use the security tables(users, authorities) which we previously created in the database to check the authentication
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-
-        UserDetails loz = User.builder()
-                .username("loz")
-                .password("{noop}loz123")
-                .roles("EMPLOYEE")
-                .build();
-
-
-        UserDetails loza = User.builder()
-                .username("loza")
-                .password("{noop}loza123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails desha = User.builder()
-                .username("desha")
-                .password("{noop}mudesha") //noop [Encryption Algorithm]=> No Operation {plain text}
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-
-        return new InMemoryUserDetailsManager(loz, loza, desha);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -52,6 +61,7 @@ public class SecurityConfig {
                     .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
                     .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
                     .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
                     .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN");
 
         });
